71ebfd5bf1a0b6deff6432ce6b5a64c43f0f150a
FarahMurtaza <104267368+FarahMurtaza@users.noreply.github.com>
2025-02-07 15:10:18 +0100

    Update avoa.h

--- avoa.h
+++ avoa.h
@@ -3,14 +3,18 @@
 
 #include <vector>
 
-double LevyFlight();
+double LevyFlight(int iteration, int maxIterations, double lowerBound, double upperBound, double beta = 1.5);
 void Eq6(double& P_i, double R_i, double F);
 void Eq8(double& P_i, double R_i, double F);
+double Eq4(double rand, double z, int iteration, int maxIterations, double h, double w);
 double Eq11(double R_i, double P_i);
 void Eq13(double& P_i, double R_i);
 void Eq10(double& P_i, double R_i, double F);
 void Eq16(double& P_i, double A1, double A2);
-void Eq17(double& P_i, double R_i, double F);
-void AVOA_MPI(int rank, int size);
+void Eq17(double &P_i, double R_i, double F, int iteration, int maxIterations, double lowerBound, double upperBound);
+void AVOA_MPI(int rank, int size, int functionChoice, int populationSize, int maxIterations, double L1, double L2, double w, double P1, double P2, double P3, int dimension);
+void AVOA_Optimization(int rank, int size, int functionChoice, int populationSize, int maxIterations, double L1, double L2, double w, double P1, double P2, double P3);
+
+
 
 #endif // AVOA_H
